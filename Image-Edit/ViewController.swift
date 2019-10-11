227cbdcde60c86321ca59b4534e80e4f0b68f855
takehiro watanabe <takehiro_watanabe@shimapri.jp>
2019-10-11 13:06:53 +0900

    Initial Commit

---
+++ Image-Edit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image-Edit
+//
+//  Created by 渡邊 丈洋 on 2019/10/11.
+//  Copyright © 2019 tkhrname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ Image-EditTests/Image_EditTests.swift
@@ -0,0 +1,34 @@
+//
+//  Image_EditTests.swift
+//  Image-EditTests
+//
+//  Created by 渡邊 丈洋 on 2019/10/11.
+//  Copyright © 2019 tkhrname. All rights reserved.
+//
+
+import XCTest
+@testable import Image_Edit
+
+class Image_EditTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
